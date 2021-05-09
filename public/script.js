a4b4f15fe3252dddc059bd62226cc714c9a8cd51
Mahnoor-Anwar <mahnoordev@gmail.com>
2021-05-09 16:20:45 +0500

    porfolio

---
+++ public/script.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('download');
+
+button.onclick = downloadFile;
+
+function downloadFile() {
+    
+        window.open("Mahnoor_Anwar_VisualCV_Resume.pdf");
+    
+}
